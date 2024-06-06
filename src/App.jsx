f0d8fa7b46f72acd41e62733c68a05592221faec
HasanC14 <chowdhuryh999@gmail.com>
2024-06-07 04:27:22 +0600

    footer, copy added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import { FaFacebook, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { FaCopy, FaFacebook, FaGithub, FaLinkedinIn } from "react-icons/fa";
 function App() {
   const [activeTab, setActiveTab] = useState("Rewrite");
   const [inputText, setInputText] = useState("");
@@ -24,6 +24,12 @@ function App() {
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center p-4">
+      <div className="flex flex-col items-center my-4">
+        <img src="/public/logo.png" alt="" className="w-40" />
+        {/* <div style={{ fontFamily: "Quicksand" }} className="text-xl">
+          Write Smart, Write Right
+        </div> */}
+      </div>
       {/* Buttons */}
       <div className="flex space-x-4 mb-4">
         {["Rewrite", "Grammar", "Formal", "Summarize", "Creative"].map(
@@ -31,7 +37,7 @@ function App() {
             <button
               key={tab}
               className={`px-4 py-2 rounded ${
-                activeTab === tab ? "bg-blue-500 text-white" : "bg-gray-200"
+                activeTab === tab ? "bg-prime text-white" : "bg-gray-200"
               }`}
               onClick={() => handleTabClick(tab)}
             >
@@ -41,7 +47,7 @@ function App() {
         )}
       </div>
       {/* Text Fields */}
-      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 w-full max-w-7xl">
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 w-full max-w-7xl relative">
         <textarea
           className="flex-1 p-4 border rounded-md resize-none h-64 md:h-96 text-sm"
           placeholder={`To ${activeTab.toLowerCase()} text, enter or paste it here and press "paraphrase"`}
@@ -49,11 +55,19 @@ function App() {
           onChange={handleInputChange}
         />
         <textarea
-          className="flex-1 p-4 border rounded-md resize-none h-64 md:h-96 text-sm"
+          className="flex-1 p-4 border rounded-md resize-none h-64 md:h-96 text-sm "
           placeholder="The result will appear here"
           value={outputText}
           readOnly
         />
+        <div className="absolute top-1 right-1">
+          <button
+            className="p-3 text-xl  text-gray-500 rounded"
+            onClick={() => navigator.clipboard.writeText(outputText)}
+          >
+            <FaCopy />
+          </button>
+        </div>
       </div>
       {/* Footer */}
       <div className="text-xs mt-10">
@@ -66,32 +80,35 @@ function App() {
           Hasan Chowdhury
         </a>
       </div>
-      <div className="flex space-x-2 mt-1 hover:*:scale-110 *transition-all *ease-in-out *duration-700 ">
+      <div className="flex space-x-2 mt-1 hover:*:scale-110 ">
         <a
           href="https://www.facebook.com/dev.hasanchowdhury"
           className="cursor-pointer"
+          target="_blank"
         >
           <FaFacebook />
         </a>
-        <a href="" className="cursor-pointer">
+        <a
+          href="https://github.com/HasanC14/new-WriteRight-client"
+          className="cursor-pointer"
+          target="_blank"
+        >
           <FaGithub />
         </a>
-        <a href="" className="cursor-pointer">
+        <a
+          href="https://www.linkedin.com/in/dev1hasanchowdhury/"
+          className="cursor-pointer"
+          target="_blank"
+        >
           <FaLinkedinIn />
         </a>
       </div>
       <div className="flex space-x-4 mt-4">
         <button
-          className="px-4 py-2 bg-blue-500 text-white rounded"
+          className="px-4 py-2 bg-prime text-white rounded"
           onClick={handleParaphrase}
         >
           Paraphrase
-        </button>
-        <button
-          className="px-4 py-2 bg-gray-500 text-white rounded"
-          onClick={() => navigator.clipboard.writeText(outputText)}
-        >
-          Copy
         </button>
       </div>
     </div>
