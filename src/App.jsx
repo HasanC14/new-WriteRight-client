178437bb92b146e08e2e9b0fd6f0589e03247352
HasanC14 <chowdhuryh999@gmail.com>
2024-06-07 06:31:41 +0600

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,175 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { FaCopy, FaFacebook, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import axios from "axios";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/bundle";
+
+import { Navigation } from "swiper/modules";
+import { FaCheck } from "react-icons/fa6";
+const filters = [
+  { text: "Rewrite", endpoint: "rewrite" },
+  { text: "Fluency", endpoint: "fluency" },
+  { text: "Natural", endpoint: "natural" },
+  { text: "Formal", endpoint: "formal" },
+  { text: "Academic", endpoint: "academic" },
+  { text: "Simple", endpoint: "simple" },
+  { text: "Creative", endpoint: "creative" },
+  { text: "Expand", endpoint: "expand" },
+  { text: "Concise", endpoint: "shorten" },
+  { text: "Grammar", endpoint: "grammar" },
+  { text: "Summarize", endpoint: "summarize" },
+];
 function App() {
-  const [activeTab, setActiveTab] = useState("Rewrite");
+  const [activeTab, setActiveTab] = useState("");
   const [inputText, setInputText] = useState("");
   const [outputText, setOutputText] = useState("");
+  const [Loading, setLoading] = useState(false);
+  const [Copy, setCopy] = useState(false);
+  const [slidesPerView, setSlidesPerView] = useState(3);
 
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
+  const handleTabClick = async (endpoint, text) => {
+    setCopy(false);
+    setLoading(true);
+    setActiveTab(text);
     setOutputText("");
+    if (inputText) {
+      await axios
+        .post(
+          `https://write-right-server-p4f3-hasanc14-hasanc14s-projects.vercel.app/${endpoint}`,
+          {
+            inputValue: inputText,
+          }
+        )
+        .then((response) => {
+          setOutputText(response.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    } else {
+      setOutputText("Write something first 😒 ");
+    }
   };
 
   const handleInputChange = (e) => {
     setInputText(e.target.value);
   };
 
-  const handleParaphrase = () => {
-    // Here you would make the API call
-    setOutputText(
-      "The result is not valid due to skill issues by the developer"
-    );
+  const HandleCopy = () => {
+    navigator.clipboard.writeText(outputText);
+    setCopy(true);
   };
+
+  const handleResize = () => {
+    if (window.innerWidth <= 350) {
+      setSlidesPerView(2);
+    } else if (window.innerWidth <= 550) {
+      setSlidesPerView(3);
+    } else if (window.innerWidth <= 720) {
+      setSlidesPerView(5);
+    } else {
+      setSlidesPerView(7);
+    }
+  };
+  window.addEventListener("resize", handleResize);
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center p-4">
-      <div className="flex flex-col items-center my-4">
-        <img src="/public/logo.png" alt="" className="w-40" />
-        {/* <div style={{ fontFamily: "Quicksand" }} className="text-xl">
-          Write Smart, Write Right
-        </div> */}
-      </div>
       {/* Buttons */}
-      <div className="flex space-x-4 mb-4">
-        {["Rewrite", "Grammar", "Formal", "Summarize", "Creative"].map(
-          (tab) => (
-            <button
-              key={tab}
-              className={`px-4 py-2 rounded ${
-                activeTab === tab ? "bg-prime text-white" : "bg-gray-200"
-              }`}
-              onClick={() => handleTabClick(tab)}
-            >
-              {tab}
-            </button>
-          )
-        )}
+      <div className="hidden lg:flex flex-wrap gap-2 mb-4 text-sm p-2 max-w-full">
+        {filters.map((filter, index) => (
+          <button
+            key={index}
+            className={`px-4 py-2 rounded hover:scale-110 transition-all ease-in-out duration-700 ${
+              activeTab === filter.text
+                ? "bg-prime text-white scale-125"
+                : "bg-gray-100"
+            }`}
+            onClick={() => handleTabClick(filter.endpoint, filter.text)}
+          >
+            {filter.text}
+          </button>
+        ))}
+      </div>
+      <div className="lg:hidden mb-4 text-xs p-2 max-w-full">
+        <Swiper spaceBetween={10} slidesPerView={slidesPerView}>
+          {filters.map((filter, index) => (
+            <SwiperSlide key={index}>
+              <button
+                className={`px-4 py-2 rounded hover:scale-110 transition-all ease-in-out duration-700 ${
+                  activeTab === filter.text
+                    ? "bg-prime text-white scale-125 "
+                    : "bg-gray-100"
+                }`}
+                onClick={() => handleTabClick(filter.endpoint, filter.text)}
+              >
+                {filter.text}
+              </button>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
       {/* Text Fields */}
-      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 w-full max-w-7xl relative">
-        <textarea
-          className="flex-1 p-4 border rounded-md resize-none h-64 md:h-96 text-sm"
-          placeholder={`To ${activeTab.toLowerCase()} text, enter or paste it here and press "paraphrase"`}
-          value={inputText}
-          onChange={handleInputChange}
-        />
-        <textarea
-          className="flex-1 p-4 border rounded-md resize-none h-64 md:h-96 text-sm "
-          placeholder="The result will appear here"
-          value={outputText}
-          readOnly
-        />
-        <div className="absolute top-1 right-1">
-          <button
-            className="p-3 text-xl  text-gray-500 rounded"
-            onClick={() => navigator.clipboard.writeText(outputText)}
-          >
-            <FaCopy />
-          </button>
+      <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 w-full max-w-7xl relative">
+        <div className="md:w-1/2 w-full">
+          <textarea
+            className="flex-1 p-4 border rounded-md resize-none h-64 md:h-96 text-sm w-full bg-gray-100"
+            placeholder="Write Smart, Write Right 😌"
+            value={inputText}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="md:w-1/2 w-full">
+          <textarea
+            className="flex-1 p-4 md:pe-10 pe-0 pt-7 border rounded-md resize-none h-64 md:h-96 text-sm w-full bg-gray-50"
+            placeholder={
+              Loading
+                ? "Thinking... 🤔"
+                : "Paraphrased content will appear here"
+            }
+            value={outputText}
+            readOnly
+          />
+          <div className="absolute md:top-1 top-1/2 right-1">
+            <button
+              className="p-3 md:text-xl text-sm  text-prime rounded"
+              onClick={HandleCopy}
+            >
+              {Copy ? (
+                <div className="flex space-x-1 items-center">
+                  <FaCheck />
+                  <span className="text-xs">Copied</span>
+                </div>
+              ) : (
+                <div className="flex space-x-1 items-center">
+                  <FaCopy />
+                  <span className="text-xs">Copy</span>
+                </div>
+              )}
+            </button>
+          </div>
         </div>
       </div>
       {/* Footer */}
-      <div className="text-xs mt-10">
-        Made with ❤️ by{" "}
-        <a
-          href="https://www.linkedin.com/in/dev1hasanchowdhury/"
-          className="text-prime hover:underline"
-          target="_blank"
-        >
-          Hasan Chowdhury
-        </a>
+      <div className="text-xs mt-10 flex flex-col md:flex-row items-center">
+        <div className="text-prime text-lg md:mr-2 mr-0">WriteRight</div>
+        <div>
+          Made with ❤️ by
+          <a
+            href="https://www.linkedin.com/in/dev1hasanchowdhury/"
+            className="text-prime hover:underline"
+            target="_blank"
+          >
+            Hasan Chowdhury
+          </a>
+        </div>
       </div>
-      <div className="flex space-x-2 mt-1 hover:*:scale-110 ">
+      <div className="flex space-x-2 mt-1 hover:*:scale-110 text-lg text-prime">
         <a
           href="https://www.facebook.com/dev.hasanchowdhury"
           className="cursor-pointer"
@@ -102,14 +191,6 @@ function App() {
         >
           <FaLinkedinIn />
         </a>
-      </div>
-      <div className="flex space-x-4 mt-4">
-        <button
-          className="px-4 py-2 bg-prime text-white rounded"
-          onClick={handleParaphrase}
-        >
-          Paraphrase
-        </button>
       </div>
     </div>
   );
